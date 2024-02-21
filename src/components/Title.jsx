87477795f4a647638a5cec5b28c45ceac715bcc8
RafaFigueiroa <rafael.00000847851@unicap.br>
2024-02-21 11:39:38 -0300

    correção nome

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Title(){
     return(
-        <h1 className="text-3xl text-sky-500">Yetflix</h1>
+        <h1 className="text-3xl text-sky-500">Nuíter</h1>
     );
 }
 
