2529b73433285e0b6a7b0079b324f90fbed7dbe7
RafaFigueiroa <rafael.00000847851@unicap.br>
2024-02-25 15:22:34 -0300

    adição do cadastro de contas a partir do firebase

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,13 +1,36 @@
-import React from "react";
 import { useForm } from 'react-hook-form'
 import classNames from "classnames";
+import { auth } from "../services/firebaseConfig";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 function UserForm(props){
-    const { register, handleSubmit, formState: {errors}} = useForm();
-    const onSubmit = (data) => {
-        console.log(data);
+    //authentication firebase hook
+    const [ 
+        createUserWithEmailAndPassword, 
+        user, loading, error, 
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    //form hook
+    const { register, handleSubmit, 
+        formState: {errors}
+    } = useForm();
+
+    //creating user
+    const onSubmit = async (data) => {
+        try{
+            await createUserWithEmailAndPassword(data.email, data.password);
+            console.log("Usuário registrado com sucesso!")
+        }
+        catch(error){
+            console.error("Erro ao registrar usuário: ", error.message);
+        }
+    };
+
+    if(loading){
+        return <p>Carregando...</p>
     }
 
+    //conditional styling
     const invalidEmail = classNames("border-2", {
         "border-gray-300": !errors.email,
         "border-red-500 bg-red-100": errors.email 
@@ -19,19 +42,27 @@ function UserForm(props){
     
     return(
         <form onSubmit={handleSubmit(onSubmit)} className="w-96 grid col-span-1 gap-2 *:rounded *:h-11 *:pl-3">
-                    <input {...register("email", {required: true})} 
-                        type="email" id="emailInput" placeholder="email@exemplo.com" 
-                        className={invalidEmail} 
-                    />
-                    
-                    <input {...register("password", {required: true, minLength: 8})} 
-                        type="password" id="passwordInput" placeholder="senha" 
-                        className={invalidPassword}
-                    />
+            <input 
+                {...register("email", {required: true})} 
+                type="email" 
+                id="emailInput" 
+                placeholder="email@exemplo.com" 
+                className={invalidEmail} 
+            />
+            
+            <input 
+                {...register("password", {required: true, minLength: 8})} 
+                type="password" 
+                id="passwordInput" 
+                placeholder="senha" 
+                className={invalidPassword}
+            />
 
-                    <input type="submit" value={props.buttonValue} 
-                        className="text-slate-100 bg-emerald-500 hover:bg-emerald-600 duration-200 mt-4" 
-                    />
+            <input 
+                type="submit" 
+                value={props.buttonValue}
+                className="text-slate-100 bg-emerald-500 hover:bg-emerald-600 duration-200 mt-4" 
+            />
         </form>
     );
 }
